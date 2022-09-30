a0b4dd6dc1b0afa14e184a0dd22692bc0d1293d1
LeixB <abone9999@gmail.com>
2022-10-01 01:36:04 +0200

    Clean up flake

--- flake.nix
+++ flake.nix
@@ -34,6 +34,20 @@
           overlays = [ inputs.poetry2nixFlake.overlay ];
         };
 
+        nix-filter = inputs.nix-filter.lib;
+        python = pkgs.python3;
+
+        overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: {
+          mypy = super.mypy.overridePythonAttrs (old: { patches = [ ]; });
+          pytoolconfig = super.pytoolconfig.overridePythonAttrs (old: { nativeBuildInputs = old.nativeBuildInputs ++ [ self.pdm-pep517 ]; });
+          jsonschema = super.jsonschema.overridePythonAttrs (old: { nativeBuildInputs = old.nativeBuildInputs ++ [ self.hatch-fancy-pypi-readme ]; });
+          jupyter-core = super.jupyter-core.overridePythonAttrs (old: { nativeBuildInputs = old.nativeBuildInputs ++ [ self.hatchling ]; });
+          nbformat = super.nbformat.overridePythonAttrs (old: { nativeBuildInputs = old.nativeBuildInputs ++ [ self.flit ]; });
+          seaborn = super.seaborn.overridePythonAttrs (old: { nativeBuildInputs = old.nativeBuildInputs ++ [ self.flit ]; });
+          pandas-stubs = super.pandas-stubs.overridePythonAttrs (old: { nativeBuildInputs = old.nativeBuildInputs ++ [ self.poetry ]; });
+          contourpy = super.contourpy.overridePythonAttrs (old: { nativeBuildInputs = old.nativeBuildInputs ++ [ self.pybind11 ]; });
+        });
+
         lab1-data =
           let
             novels = pkgs.fetchzip {
@@ -83,29 +97,14 @@
             # Despite the name, download.sh does not download anything from the
             # internet
             installPhase = ''
+              runHook preInstall
+
               export NLTK_DATA_DIR=$out
               bash tools/download.sh "${collection}"
+
+              runHook postInstall
             '';
           };
-
-        python = pkgs.python3;
-
-        overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: {
-          mypy = super.mypy.overridePythonAttrs (old: { patches = [ ]; });
-          pytoolconfig = super.pytoolconfig.overridePythonAttrs (old: { nativeBuildInputs = old.nativeBuildInputs ++ [ self.pdm-pep517 ]; });
-          jsonschema = super.jsonschema.overridePythonAttrs (old: { nativeBuildInputs = old.nativeBuildInputs ++ [ self.hatch-fancy-pypi-readme ]; });
-          jupyter-core = super.jupyter-core.overridePythonAttrs (old: { nativeBuildInputs = old.nativeBuildInputs ++ [ self.hatchling ]; });
-          nbformat = super.nbformat.overridePythonAttrs (old: { nativeBuildInputs = old.nativeBuildInputs ++ [ self.flit ]; });
-          seaborn = super.seaborn.overridePythonAttrs (old: { nativeBuildInputs = old.nativeBuildInputs ++ [ self.flit ]; });
-          pandas-stubs = super.pandas-stubs.overridePythonAttrs (old: { nativeBuildInputs = old.nativeBuildInputs ++ [ self.poetry ]; });
-          contourpy = super.contourpy.overridePythonAttrs (old: { nativeBuildInputs = old.nativeBuildInputs ++ [ self.pybind11 ]; });
-        });
-
-        nix-filter = inputs.nix-filter.lib;
-
-        lab-list = with pkgs.lib; builtins.attrNames (filterAttrs
-          (name: type: type == "directory" && hasPrefix "lab" name)
-          (builtins.readDir "${./.}"));
 
         py-env = { groups ? [ ] }: pkgs.poetry2nix.mkPoetryEnv {
           inherit python overrides groups;
@@ -119,28 +118,12 @@
           };
         };
 
+        lab-list = with pkgs.lib; builtins.attrNames (filterAttrs
+          (name: type: type == "directory" && hasPrefix "lab" name)
+          (builtins.readDir "${./.}"));
+
       in
       {
-        devShells = {
-          default = pkgs.mkShellNoCC {
-            name = "python-poetry";
-
-            inherit NLTK_DATA;
-            DATA = lab1-data;
-
-            buildInputs = [
-              pkgs.poetry
-              (py-env { groups = [ "dev" ] ++ lab-list; })
-              pkgs.nixpkgs-fmt
-              pkgs.pandoc
-              pkgs.texlive.combined.scheme-full
-              pkgs.parallel
-            ];
-
-            inherit (self.checks.${system}.pre-commit-check) shellHook;
-          };
-        };
-
         checks = {
           pre-commit-check = pre-commit-hooks.lib.${system}.run {
             src = ./.;
@@ -165,13 +148,21 @@
                   "assignment.pdf"
                 ];
               };
-              buildInputs = [ (py-env { groups = [ name ]; }) pkgs.texlive.combined.scheme-full ];
+              buildInputs =
+                let
+                  python-env = py-env { groups = [ name ]; };
+                in
+                [ python-env pkgs.texlive.combined.scheme-full ];
               doCheck = false;
               dontInstall = true;
               buildPhase = ''
+                runHook preBuild
+
                 python3 process.py --output $out/figures --format=pdf
                 cp -r tables $out || true
                 cp -r images $out || true
+
+                runHook postBuild
               '';
             };
 
@@ -219,6 +210,10 @@
             lab-figures = builtins.map build-figures lab-list;
             lab-all = pkgs.lib.zipListsWith bundle-deliverable lab-reports lab-figures;
             lab-deliverables = builtins.map zip-derivation lab-all;
+
+            derivations = with builtins; listToAttrs (
+              map (drv: pkgs.lib.nameValuePair drv.name drv)
+                (concatLists [ lab-reports lab-figures lab-deliverables lab-all ]));
           in
           {
             default = self.packages.${system}.reports;
@@ -241,8 +236,30 @@
               "labs"
               lab-all;
 
-          } // (with builtins; listToAttrs (
-            map (drv: pkgs.lib.nameValuePair drv.name drv)
-              (concatLists [ lab-reports lab-figures lab-deliverables lab-all ])));
+          } // derivations;
+
+        devShells = {
+          default = pkgs.mkShellNoCC {
+            name = "python-poetry";
+
+            inherit NLTK_DATA;
+            DATA = lab1-data;
+
+            buildInputs =
+              let
+                python-env = py-env { groups = [ "dev" ] ++ lab-list; };
+              in
+              with pkgs;[
+                poetry
+                python-env
+                nixpkgs-fmt
+                pandoc
+                texlive.combined.scheme-full
+                parallel
+              ];
+
+            inherit (self.checks.${system}.pre-commit-check) shellHook;
+          };
+        };
       });
 }
