76b39c413b8de05f0618d52774f8c3c6ef5df41d
LeixB <abone9999@gmail.com>
2022-10-11 16:32:44 +0200

    lab2: add option to filter paths out

--- lab2/TFIDF_experiment.py
+++ lab2/TFIDF_experiment.py
@@ -22,11 +22,14 @@ def worker_init(*args, **kwargs):
 
 
 class Slicer:
-    def __init__(self, index: str, n: int, tfw_orig, slices: int):
+    def __init__(
+        self, index: str, n: int, tfw_orig, slices: int, ignore_paths: List[str]
+    ):
         self.index = index
         self.n = n
         self.slices = slices
         self.tfw_orig = tfw_orig
+        self.ignore_paths = ignore_paths
 
     def process_slice(self, slice_no: int) -> List[Tuple[float, str]]:
         global client
@@ -37,7 +40,15 @@ class Slicer:
             .scan()
         )
 
-        doc_ids = map(lambda f: f.meta.id, sc)
+        def filter_path(hit):
+            for path in self.ignore_paths:
+                if hit.path.startswith(path):
+                    return False
+            return True
+
+        doc_ids = filter(filter_path, sc)
+
+        doc_ids = map(lambda f: f.meta.id, doc_ids)
         scores = map(
             lambda doc: (
                 cosine_similarity(self.tfw_orig, toTFIDF(_client, self.index, doc)),
@@ -49,8 +60,10 @@ class Slicer:
         return nlargest(self.n, scores)
 
 
-def main(index: str, path: str, n: int):
+def main(index: str, path: str, n: int, ignore_paths: List[str], slices: int):
     client = Elasticsearch(timeout=1000)
+
+    print(ignore_paths)
 
     doc_original = search_file_by_path(client, index, path)
 
@@ -62,12 +75,10 @@ def main(index: str, path: str, n: int):
     keywords = [x[0] for x in nlargest(10, tfw_orig, key=lambda x: x[1])]
     print("Keywords:", ", ".join(keywords))
 
-    SLICES = cpu_count() - 1
+    slicer = Slicer(index, n, tfw_orig, slices, ignore_paths)
 
-    slicer = Slicer(index, n, tfw_orig, SLICES)
-
-    with Pool(SLICES, initializer=worker_init) as p:
-        results = p.map(slicer.process_slice, range(SLICES))
+    with Pool(slices, initializer=worker_init) as p:
+        results = p.map(slicer.process_slice, range(slices))
 
     results = nlargest(n, chain(*results), key=lambda x: x[0])
 
@@ -80,13 +91,32 @@ def main(index: str, path: str, n: int):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Search for a file using its path")
-    parser.add_argument("index", type=str, help="index to search in")
+    parser.add_argument("--index", type=str, help="index to search in")
     parser.add_argument("path", type=str, help="path of the file to search")
+    parser.add_argument(
+        "--ignore-paths",
+        type=str,
+        help="ignore paths containing this strings",
+        nargs="+",
+        default=[],
+    )
+    parser.add_argument(
+        "--nproc",
+        type=int,
+        help="Number of processes to use",
+        default=cpu_count() - 1,
+    )
     parser.add_argument("-n", type=int, default=10, help="number of results to return")
     args = parser.parse_args()
 
     try:
-        main(args.index, args.path, args.n)
+        main(
+            args.index,
+            args.path,
+            args.n,
+            ignore_paths=args.ignore_paths,
+            slices=args.nproc,
+        )
     except NotFoundError:
         print("Not found", file=sys.stderr)
         sys.exit(1)
