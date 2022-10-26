d42ac4591d69b403378a31419d997b29aadfe019
LeixB <abone9999@gmail.com>
2022-10-26 14:09:32 +0200

    lab3: stubs

---
+++ lab3/process.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Auxiliary script to build figures and plots for the report
+# This should only rely on the resources available in the repository
+# so that figures can be reproduced with nix
+
+import argparse
+import pathlib
+
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("--format", type=str, default="pdf", choices=["png", "pdf"])
+    arg_parser.add_argument(
+        "--output", type=str, default="figures", help="Output folder for plots"
+    )
+
+    args = arg_parser.parse_args()
+
+    pathlib.Path(args.output).mkdir(parents=True, exist_ok=True)
