73923e5ea3527d6c8e596af2f042b3b8f0413fd5
LeixB <abone9999@gmail.com>
2022-11-23 13:09:52 +0100

    lab4: init

---
+++ lab4/MRWordCount.py
@@ -0,0 +1,21 @@
+"""
+WordCountMR
+"""
+import re
+
+from mrjob.job import MRJob
+
+WORD_RE = re.compile(r"[a-z]+")
+
+
+class MRWordFrequencyCount(MRJob):
+    def mapper(self, _, line):
+        for word in WORD_RE.findall(line):
+            yield word.lower(), 1
+
+    def reducer(self, key, values):
+        yield key, sum(values)
+
+
+if __name__ == "__main__":
+    MRWordFrequencyCount.run()
